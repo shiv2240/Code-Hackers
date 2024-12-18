ce99a87e0a17b9fd5fdd08333ff85ec4f096464e
AdityaKumarThakur12 <aditya3863297@gmail.com>
2024-12-18 13:46:08 +0530

    font end part of application

---
+++ Crowdfunding/src/App.jsx
@@ -0,0 +1,28 @@
+import Home from './Components/home'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import About from './Components/about'
+import Blog from './Components/blog'
+import Pages from './Components/pages'
+import Contact from './Components/contact'
+
+function App() {
+
+
+  return (
+   <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/pages' element={<Pages/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
+   </>
+  )
+}
+
+export default App
---
+++ Crowdfunding/src/Components/about.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./navbar"
+
+const About = ()=>{
+    return(
+        <div>
+            <Navbar/>
+            <h1>Welcome to About page</h1>
+        </div>
+    )
+}
+export default About
